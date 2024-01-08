d2c1d9c83b513c1a2baede3592bab79e3428d577
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-01-08 14:41:54 -0300

    Modificando cors

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
